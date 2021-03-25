34886469c07348e962e91582c19228cd6b269a05
karin knutsson <karinmiriam@gmail.com>
2021-03-25 20:53:41 +0100

    set up form for sprint intervals

--- modules/intervals.js
+++ modules/intervals.js
@@ -1,9 +1,28 @@
 const modal = document.getElementById('intervalModal');
 const span = document.getElementsByClassName('close')[0];
 
+
+function intervalsForm() {
+  const form = document.getElementById('interval-form');
+  const sprintLength = document.getElementById('sprint-input');
+  const pauseLength = document.getElementById('pause-input');
+  const repCount = document.getElementById('rep-input');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+
+
+
+  });
+}
+
+
 function openIntervals(e) {
   if (e.keyCode == 83) {
     modal.style.display = 'block';
+
+    intervalsForm();
 
     span.onclick = function() {
       modal.style.display = 'none';
